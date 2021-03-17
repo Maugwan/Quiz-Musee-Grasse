6f25e47b72ab98b497257d2368bf93bb18320fe4
LACHATRE Maugwan <lachatre.maugwan@gmail.com>
2021-03-17 21:41:04 +0100

    Hover
    Add hover effect to Selection (img on div)

--- Projet/Prototype/src/App.js
+++ Projet/Prototype/src/App.js
@@ -26,7 +26,7 @@ function App() {
         </div>
           <div className="Description"> 
             <div className="DescriptionTitle"><h3>Description :</h3></div>
-            <div className="DescriptionContent"><p></p></div>
+            <div className="DescriptionContent"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
           </div>
         <div className="Footer">
           <p className="center">Designed by EPITECH</p>
