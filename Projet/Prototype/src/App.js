e4645208b246bdd3f5e6534562a02b83e468f59e
LACHATRE Maugwan <lachatre.maugwan@gmail.com>
2021-03-18 13:18:09 +0100

    Description
    Add Description array to stock text and change when div is on hover

--- Projet/Prototype/src/App.js
+++ Projet/Prototype/src/App.js
@@ -5,18 +5,28 @@ function App() {
 
   const questions = [     //Le tableau contient les questions(bientot les reponses aussi)
     {
-      questionText: '1 Quelle plante utilise-t-on pour parfumer les poudres ?',
+      id: 1,
+      questionText: <h1>1 Quelle plante utilise-t-on pour parfumer les poudres ?</h1>,
     },
     {
       questionText: '2 Test question 2',
     }
   ]
+
+  const description = [
+    {
+      id: 1,
+      descriptionText: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'</p>
+    }
+  ]
+
+  var questionsNumber = 0;
   
   return (
     <div className="App">
 
       <div className="Container">
-      <div className="QuestionText"> <div className="Question"><h1>{questions[0].questionText} </h1></div></div> 
+      <div className="QuestionText"> <div className="Question">{questions[questionsNumber].questionText}</div></div> 
 
         <div className="QuestionImages">
         <div className="Selection Graines"></div>
@@ -26,7 +36,7 @@ function App() {
         </div>
           <div className="Description"> 
             <div className="DescriptionTitle"><h3>Description :</h3></div>
-            <div className="DescriptionContent"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+            <div className="DescriptionContent">{description[0].descriptionText}</div>
           </div>
         <div className="Footer">
           <p className="center">Designed by EPITECH</p>
