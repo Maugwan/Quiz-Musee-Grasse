4a8a23d0f293020f4c0039d962655d064d959c76
LACHATRE Maugwan <lachatre.maugwan@gmail.com>
2021-04-08 17:12:51 +0200

    Changement Images
    Add image change on dbClick.
    Hooks
    
    Missing question 6, Score counter

--- Projet/Prototype/src/App.js
+++ Projet/Prototype/src/App.js
@@ -1,25 +1,131 @@
 import './css/App.css';
-import Counter from './posts/Counter'
-//import Description from './posts/Description'
+import './images/Questions/1.jpg'
+import './images/Questions/2.jpg'
+import React, { useEffect, useState } from "react";
 import PostData from './data/data.json'
-import './css/Selection.css' //Style de Css test pour la question 1 en attendant les ressources finales
+import './css/Selection.css'
 
 function App() {
+  let [question, setQuestion] = useState(0);
+  let [questionCount, setQuestionCount] = useState(0);
+  let [description, setDescription] = useState(0);
+  let [imagesCount, setImagesCount] = useState(0);
 
-  var questionsNumber = 0;
-  var answersNumber = 0;
+      const increment = () =>{
+        setQuestion(question + 1)
+    };
+    
+      const imagesBackGround = [
+        {
+          questionBackGroung: 'question ImagesOne',
+          answersOne: 'Selection answerOne oneOne',
+          answersTwo: 'Selection answerTwo oneTwo',
+          answersThree: 'Selection answerThree oneThree',
+          answersFour: 'Selection answerFour oneThree'
+        },
+        {
+          questionBackGroung: 'question ImagesTwo',
+          answersOne: 'Selection answerOne twoOne',
+          answersTwo: 'Selection answerTwo twoTwo',
+          answersThree: 'Selection answerThree twoThree',
+          answersFour: 'Selection answerFour twoFour'
+        },
+        {
+          questionBackGroung: 'question ImagesThree',
+          answersOne: 'Selection answerOne threeOne',
+          answersTwo: 'Selection answerTwo threeTwo',
+          answersThree: 'Selection answerThree threeThree',
+          answersFour: 'Selection answerFour threeFour'
+        },
+        {
+          questionBackGroung: 'question ImagesFour',
+          answersOne: 'Selection answerOne fourOne',
+          answersTwo: 'Selection answerTwo fourTwo',
+          answersThree: 'Selection answerThree fourThree',
+          answersFour: 'Selection answerFour fourFour'
+        },
+        {
+          questionBackGroung: 'question ImagesFive',
+          answersOne: 'Selection answerOne fiveOne',
+          answersTwo: 'Selection answerTwo fiveTwo',
+          answersThree: 'Selection answerThree fiveThree',
+          answersFour: 'Selection answerFour fiveFour'
+        },
+        {
+          questionBackGroung: 'question ImagesSix',
+          answersOne: 'Selection answerOne fourSeven',
+          answersTwo: 'Selection answerTwo fourSeven',
+          answersThree: 'Selection answerThree fourSeven',
+          answersFour: 'Selection answerFour fourSeven'
+        },
+        {
+          questionBackGroung: 'question ImagesSeven',
+          answersOne: 'Selection answerOne fourSeven',
+          answersTwo: 'Selection answerTwo fourSeven',
+          answersThree: 'Selection answerThree fourSeven',
+          answersFour: 'Selection answerFour fourSeven'
+        },
+        {
+          questionBackGroung: 'question ImagesHeight',
+          answersOne: 'Selection answerOne heightOne',
+          answersTwo: 'Selection answerTwo heightTwo',
+          answersThree: 'Selection answerThree heightThree',
+          answersFour: 'Selection answerFour heightFour'
+        },
+        {
+          questionBackGroung: 'question ImagesNine',
+          answersOne: 'Selection answerOne nineOne',
+          answersTwo: 'Selection answerTwo nineTwo',
+          answersThree: 'Selection answerThree nineThree',
+          answersFour: 'Selection answerFour nineFour'
+        },
+        {
+          questionBackGroung: 'question ImagesTen',
+          answersOne: 'Selection answerOne tenOne',
+          answersTwo: 'Selection answerTwo tenTwo',
+          answersThree: 'Selection answerThree tenThree',
+          answersFour: 'Selection answerFour tenFour'
+        },
+        {
+          questionBackGroung: 'question ImagesEleven',
+          answersOne: 'Selection answerOne elevenOne',
+          answersTwo: 'Selection answerTwo elevenTwo',
+          answersThree: 'Selection answerThree elevenThree',
+          answersFour: 'Selection answerFour elevenFour'
+        },
+        {
+          questionBackGroung: 'question ImagesTwelve',
+          answersOne: 'Selection answerOne twelveOne',
+          answersTwo: 'Selection answerTwo twelveTwo',
+          answersThree: 'Selection answerThree twelveThree',
+          answersFour: 'Selection answerFour twelveFour'
+        },
+        {
+          questionBackGroung: 'question ImagesThirteen',
+          answersOne: 'Selection answerOne thirteenOne',
+          answersTwo: 'Selection answerTwo thirteenTwo',
+          answersThree: 'Selection answerThree thirteenThree',
+          answersFour: 'Selection answerFour thirteenFour'
+        },
+      ]
 
   return (
     <div className="App">
 
       <div className="Container">
-      <div className="QuestionText"><h2>{PostData[0].question}</h2></div> 
-        <div className="QuestionImages"><Counter/></div>
+      <div className="QuestionText"><h2>{PostData[question].question}</h2></div> 
+        <div className={imagesBackGround[question].questionBackGroung}>
+                <div className={imagesBackGround[question].answersOne} onMouseEnter={() => setDescription(description = 0)} onDoubleClick={increment}></div>
+                <div className={imagesBackGround[question].answersTwo} onMouseEnter={() => setDescription(description = 1)} onDoubleClick={increment}></div>
+                <div className={imagesBackGround[question].answersThree} onMouseEnter={() => setDescription(description = 2)} onDoubleClick={increment}></div>
+                <div className={imagesBackGround[question].answersFour} onMouseEnter={() => setDescription(description = 3)} onDoubleClick={increment}></div>
+        </div>
 
           <div className="Description">
             <div className="DescriptionTitle"><h3>Description :</h3></div>
-            <div className="DescriptionContent">{PostData[questionsNumber].answers[answersNumber].description}</div>
+            <div className="DescriptionContent">{PostData[question].answers[description].description}</div>
           </div>
+          
         <div className="Footer">
           <p className="center">Designed by EPITECH</p>
         </div>
--- Projet/Prototype/src/posts/Counter.js
+++ Projet/Prototype/src/posts/Counter.js
@@ -1,29 +1,34 @@
 import { Component } from "react";
 
-class Counter extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0
-        }
-    }
-    increment = () =>{
+function Counter {
+
+    const increment = () =>{
         let currentCount = this.state.count
         this.setState({
             count: currentCount + 1
         })
     };
-    decrement = () =>{
+    const decrement = () =>{
         this.setState({
             count: this.state.count - 1
         })
     };
 
-    render() {
+    const testNumber = () => {
+        let answer = this.state.test
+        this.setState({
+            test: answer +1
+        })
+    }
+
+ 
         return (
             <div>
                 <div className='Selection five' onClick={this.increment}>
                 <p>My counter: {this.state.count}</p>
+                </div>
+                <div className='Selection six' onClick={this.increment}>
+                <p>My score: {this.state.count}</p>
                 </div>
                 <div className="Selection one" onClick={this.increment}></div>
                 <div className="Selection two" onClick={this.increment}></div>
@@ -31,7 +36,6 @@ class Counter extends Component {
                 <div className="Selection four" onMouseEnter={this.increment}></div>
             </div>
         );
-    } 
 }
 
 export default Counter;
